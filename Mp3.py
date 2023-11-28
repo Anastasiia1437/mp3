448d8f1a6520bee1d7d989eb24aca354aeacdaec
N3s4a <nesakrad56@gmail.com>
2023-11-28 09:28:30 +0100

    dodanie plików, próby napisania programu

---
+++ Mp3.py
@@ -0,0 +1,6 @@
+from tkinter import *
+import subprocess
+import os
+window = Tk()
+os.system
+window.mainloop()
---
+++ test.bat
@@ -0,0 +1,6 @@
+@echo off
+cd "C:\Python311\"
+python.exe "C:\Users\2dTp gr1\Documents\mp3\mp3\plik1.py"
+code "C:\Users\2dTp gr1\Documents\mp3\mp3\plik1.py"
+python.exe "C:\Users\2dTp gr1\Documents\mp3\mp3\plik1.py"
+pause
