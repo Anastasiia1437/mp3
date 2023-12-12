52a916d59e7dc420cf7b82ef9e14e9de01a7651f
N3s4a <nesakrad56@gmail.com>
2023-12-12 22:52:20 +0100

    dalsze poprawki

--- plik2.py
+++ plik2.py
@@ -6,24 +6,28 @@ class SimpleMP3Player:
     def __init__(self, master):
         self.master = master
         master.title("Odtwarzacz MP3")
-        master.geometry("660x300")
+        master.geometry("679x500")
         master.configure(bg="black")
+        master.resizable(False, False)
 
         self.current_file = None
         self.playing = False
-
-        text_frame = Frame(master, bg="black", pady=5)
+#Frames po lewej
+        text_frame = Frame(master, bg="pink",pady=5)
         text_frame.grid(row=0, column=0, columnspan=3, sticky="nsew")
 
-        button_frame = Frame(master, bg="black")
-        button_frame.grid(row=1, column=0, columnspan=3, sticky="nsew")
+        anim_frame = Frame(master, bg="red",pady=5)
+        anim_frame.grid(row=1, column=0, columnspan=3, sticky="nsew")
 
-        volume_frame = Frame(master, bg="black")
-        volume_frame.grid(row=2, column=0, columnspan=3, sticky="nsew")
+        button_frame = Frame(master, bg="blue")
+        button_frame.grid(row=2, column=0, columnspan=3, sticky="s")
 
-        Main_frame2 = Frame(master, bg="black", padx="100", pady="70")
-        Main_frame2.grid(row=0, column=3, rowspan=3, sticky="w")
-
+        volume_frame = Frame(master, bg="red")
+        volume_frame.grid(row=3, column=0, columnspan=3, sticky="s")
+#Frame po lewej
+        List_frame = Frame(master, bg="black")
+        List_frame.grid(row=0, column=3, rowspan=4, sticky="w")
+#wybieranie folderu
         self.folder_path = filedialog.askdirectory(title="Wybierz folder")
         if self.folder_path:
             print(f"\nZawartość folderu {self.folder_path}:")
@@ -44,25 +48,25 @@ class SimpleMP3Player:
                     self.label2 = Label(text_frame, text=f"{self.mp3_files[self.current_index]}", bg="black", fg="green")
                     self.label2.grid(row=1, column=0)
 
-                    self.prev_button = Button(button_frame, text="<", command=self.play_prev, bg="gray", fg="green")
-                    self.prev_button.grid(row=1, column=0)
+                    self.prev_button = Button(button_frame, text="<", command=self.play_prev, bg="gray", fg="black", padx="20", pady="20")
+                    self.prev_button.grid(row=1, column=0, sticky="ew")
 
-                    self.play_button = Button(button_frame, text="II", command=self.play, bg="gray", fg="green")
-                    self.play_button.grid(row=1, column=1)
+                    self.play_button = Button(button_frame, text="II", command=self.play, bg="gray", fg="black", padx="20", pady="20")
+                    self.play_button.grid(row=1, column=1, sticky="ew")
 
-                    self.next_button = Button(button_frame, text=">", command=self.play_next, bg="gray", fg="green")
-                    self.next_button.grid(row=1, column=2)
+                    self.next_button = Button(button_frame, text=">", command=self.play_next, bg="gray", fg="black", padx="20", pady="20")
+                    self.next_button.grid(row=1, column=2, sticky="ew")
 
-                    self.volume_label = Label(volume_frame, text="Głośność:", bg="pink", fg="Black")
+                    self.volume_label = Label(volume_frame, text="Głośność:", bg="black", fg="green")
                     self.volume_label.grid(row=0, column=0)
 
                     self.volume_slider = Scale(volume_frame, from_=0, to=100, resolution=1, orient=HORIZONTAL,
-                                               command=self.set_volume, bg="black", fg="white", troughcolor="black",
-                                               highlightbackground="black")
+                                               command=self.set_volume, bg="black", fg="green", troughcolor="black",
+                                               highlightbackground="black", width="15", length="326")
                     self.volume_slider.set(50)  # Domyślna głośność
-                    self.volume_slider.grid(row=0, column=1)
+                    self.volume_slider.grid(row=1, column=0, sticky="nsew")
 
-                    self.listbox = Listbox(Main_frame2, selectmode="SINGLE", exportselection=0, bg="black", foreground="green")
+                    self.listbox = Listbox(List_frame, selectmode="SINGLE", exportselection=0, bg="black", foreground="green", width="56", height="31")
                     self.listbox.pack()
 
                     for mp3_file in self.mp3_files:
@@ -70,44 +74,17 @@ class SimpleMP3Player:
 
                     self.listbox.bind("<Double-Button-1>", self.play_selected)
 
+                    
+
                 else:
                     print("Brak plików MP3 w folderze.")
             except OSError as e:
                 print(f"Wystąpił błąd podczas odczytu zawartości folderu: {e}")
 
-    def play_prev(self):
-        self.current_index = (self.current_index - 1) % len(self.mp3_files)
-        self.play_current()
+   
 
-    def play_next(self):
-        self.current_index = (self.current_index + 1) % len(self.mp3_files)
-        self.play_current()
-
-    def play_current(self):
-        self.current_mp3_file = os.path.join(self.folder_path, self.mp3_files[self.current_index])
-        pygame.mixer.music.load(self.current_mp3_file)
-        pygame.mixer.music.play()
-        self.label2.config(text=f"{self.mp3_files[self.current_index]}")
-
-    def play_selected(self, event):
-        selected_index = self.listbox.curselection()
-        if selected_index:
-            self.current_index = selected_index[0]
-            self.play_current()
-
-    def play(self):
-        if self.playing:
-            pygame.mixer.music.pause()
-            self.playing = False
-        else:
-            pygame.mixer.music.unpause()
-            self.playing = True
-        aktualna_nazwa = self.play_button.cget("text")
-        nowa_nazwa = "▷" if aktualna_nazwa != "▷" else "II"
-        self.play_button.config(text=nowa_nazwa)
-
-    def set_volume(self, volume):
-        pygame.mixer.music.set_volume(float(volume) / 100)
+    #if self.playing == False:
+    #    play_next()
 
 
 if __name__ == "__main__":
