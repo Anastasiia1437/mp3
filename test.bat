71900a09b58d413b3adfeb2c2de381c9c84c4ef3
N3s4a <nesakrad56@gmail.com>
2023-12-13 11:18:22 +0100

    k

--- test.bat
+++ test.bat
@@ -3,4 +3,4 @@ cd "C:\Python311\"
 python.exe "C:\Users\2dTp gr1\Documents\mp3\mp3\plik1.py"
 code "C:\Users\2dTp gr1\Documents\mp3\mp3\plik1.py"
 python.exe "C:\Users\2dTp gr1\Documents\mp3\mp3\plik1.py"
-pause.
+pause
