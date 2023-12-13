a94e15953c9723b8d45d91d34f7800f5dda7cd03
N3s4a <nesakrad56@gmail.com>
2023-12-13 17:22:24 +0100

    k

--- test.bat
+++ test.bat
@@ -1,5 +1,5 @@
 @echo off
-cd "C:\Python311\"
+cd "C:\Python311"
 python.exe "C:\Users\2dTp gr1\Documents\mp3\mp3\plik1.py"
 code "C:\Users\2dTp gr1\Documents\mp3\mp3\plik1.py"
 python.exe "C:\Users\2dTp gr1\Documents\mp3\mp3\plik1.py"
