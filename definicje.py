aaaf4b604e2efa841b709f8fbaf5cdc9b415f880
Anastasiia1437 <tasyabondarenko840@gmail.com>
2024-01-02 07:59:26 +0100

    usunięcie niepotrzebnych plików

--- definicje.py
+++
@@ -1,4 +0,0 @@
-from tkinter import *
-from tkinter import filedialog
-import pygame
-import os
--- test.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-cd "C:\Python311\"
-python.exe "C:\Users\2dTp gr1\Documents\mp3\mp3\plik1.py"
-code "C:\Users\2dTp gr1\Documents\mp3\mp3\plik1.py"
-python.exe "C:\Users\2dTp gr1\Documents\mp3\mp3\plik1.py"
-pause
